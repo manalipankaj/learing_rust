0145a56c591abfbd4d370060c5e2a93ef5f44064
manalipankaj <pankaj.manali@gmail.com>
2018-10-18 10:14:05 +0530

    Hello world rust

---
+++ hello_world/target/debug/deps/hello_world-f87c52e2f5a4a1bc.d
@@ -0,0 +1,5 @@
+/Users/pankajmanali/practice/rust/hello_world/target/debug/deps/hello_world-f87c52e2f5a4a1bc: src/main.rs
+
+/Users/pankajmanali/practice/rust/hello_world/target/debug/deps/hello_world-f87c52e2f5a4a1bc.d: src/main.rs
+
+src/main.rs:
---
+++ hello_world/target/debug/hello_world.d
@@ -0,0 +1,1 @@
+/Users/pankajmanali/practice/rust/hello_world/target/debug/hello_world.dSYM: /Users/pankajmanali/practice/rust/hello_world/src/main.rs
