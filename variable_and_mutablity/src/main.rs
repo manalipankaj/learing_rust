374475cc1ba2f3a1e17484a163df88d862770e7e
manalipankaj <pankaj.manali@gmail.com>
2018-10-30 09:26:55 +0530

    Shadowing

--- variable_and_mutablity/src/main.rs
+++ variable_and_mutablity/src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
+    //Immutablity
     let mut x = 5;
     println!("x is {}",x);
     x = 6;
     println!("x is {}", x);
 
+    //Constant
     const MAX :u8 = 100;
     println!("MAX is constant {}", MAX);
+
+    //Shadowing
+    let y = 5;
+    let y = y + 1;
+    let y = y * 2;
+    println!("Value is x {}", y);
 }
