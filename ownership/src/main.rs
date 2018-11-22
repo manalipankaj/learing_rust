ab697f79ade8c1e6286f8cfdb82802feb870f21c
manalipankaj <pankaj.manali@gmail.com>
2018-11-22 15:37:11 +0530

    Function parameter as referrence

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -3,6 +3,10 @@ fn main() {
     ownership();
     let len = calculate_length(String::from("Hello"));
     println!("lenght of {} is {}",len.0, len.1);
+
+    let s1 = String::from("Hello");
+    let len2 = calculate_length_passing_ref( &s1 );
+    println!("length of {} is {}", s1, len2 );
 }
 
 fn ownership() {
@@ -35,4 +39,8 @@ fn calculate_length(s: String) -> (String, usize) {
     let length = s.len(); // len() returns the length of a String
 
     (s, length)
+}
+
+fn calculate_length_passing_ref(s: &String) -> usize {
+    s.len()
 }
