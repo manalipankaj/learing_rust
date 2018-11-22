a5b849955dd6c669fae522b9a804dedb2496edfe
manalipankaj <pankaj.manali@gmail.com>
2018-11-22 11:52:17 +0530

    Shadowing in premitive data type

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -16,5 +16,15 @@ fn ownership() {
     // move variable
     let s1 = String::from("hello");
     let s2 = s1;
-    println!("s1 is {}", s1);
+    // println!("s1 is {}", s1); //Will give error becuase s1 is now valid anymore
+
+    // clone data
+    let x = String::from("hello");
+    let y = x.clone();
+    println!("x is {} and y is {}", x, y);
+
+    // Why move doesn't work here
+    let a = 5;
+    let b = a;
+    println!("a is {} and b is {}", a, b);  // This works because interger is stored in stack memory
 }
